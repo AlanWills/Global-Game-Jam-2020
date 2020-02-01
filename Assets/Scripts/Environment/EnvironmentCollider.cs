2e3f7be7b58e549969f2df8f1f05c39f70d28ddf
Alan Wills <alawills@googlemail.com>
2020-02-01 20:54:50 +0000

    Started doing collisions

---
+++ Assets/Scripts/Environment/EnvironmentCollider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Celeste.Environment
+{
+    [AddComponentMenu("Celeste/Environment/Environment Collider")]
+    [RequireComponent(typeof(Collider2D))]
+    public class EnvironmentCollider : MonoBehaviour
+    {
+        #region Properties and Fields
+
+        public const string EnvironmentTag = "Environment";
+
+        #endregion
+
+        #region Collision Callbacks
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag(EnvironmentTag))
+            {
+                // Disable movement
+            }
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            
+        }
+
+        #endregion
+    }
+}
