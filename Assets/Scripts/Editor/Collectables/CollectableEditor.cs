d3ea0e717152cb131855e4062dff5b7200b3e799
Alan Wills <alawills@googlemail.com>
2020-02-01 09:37:27 +0000

    Adding collectables

---
+++ Assets/Scripts/Editor/Collectables/CollectableEditor.cs
@@ -0,0 +1,20 @@
+﻿using Celeste.Collectables;
+using Celeste.Utility;
+using UnityEditor;
+
+namespace CelesteEditor.Collectables
+{
+	[CustomEditor(typeof(Collectable))]
+	public class CollectableEditor : Editor
+	{
+        #region Component Menu Creation
+
+        [MenuItem("Assets/Create/Celeste/Collectables/Collectable")]
+		public static void Create()
+		{
+			ScriptableObjectUtility.CreateAsset<Collectable>();
+		}
+
+		#endregion
+	}
+}
