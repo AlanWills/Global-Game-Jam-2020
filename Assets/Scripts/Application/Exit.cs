31e878c651d5bbffddf44d08efd940e8f3f46758
Alan Wills <alawills@googlemail.com>
2020-02-02 16:28:32 +0000

    Added quit button

---
+++ Assets/Scripts/Application/Exit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Celeste.Application
+{
+    [AddComponentMenu("Celeste/Application/Exit")]
+    public class Exit : MonoBehaviour
+    {
+        public void Quit()
+        {
+            UnityEngine.Application.Quit();
+        }
+    }
+}
